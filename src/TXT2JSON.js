30a83ee391fcd0266e920a8f96116a9e31936bce
yr2610 <shin.nakaji@nifty.com>
2023-07-31 19:39:35 +0900

    一旦一つ元に戻す

--- src/TXT2JSON.js
+++ src/TXT2JSON.js
@@ -2597,10 +2597,7 @@ CL.deletePropertyForAllNodes(root, "marker");
             }
 
             forAllNodes_Recurse(subTree, null, -1, function(node, parent, index) {
-                // {{}} は {{$value}} を指定したものとみなす
-                node.text = node.text.replace(/\{\{\s*\}\}/g, "{{$value}}");
-
-               // あるnodeに1個でも false 的なものが渡されたら、それ以下のnode削除
+                // あるnodeに1個でも false 的なものが渡されたら、それ以下のnode削除
                 var toDelete = false;
                 function replacer(m, k) {
                     k = k.trim();
